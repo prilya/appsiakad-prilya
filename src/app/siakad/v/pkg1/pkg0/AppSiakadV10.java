6cbb377416c2982a5e2a3567c9591bde233176b8
Mareta <maretahandanari@gmail.com>
2018-10-16 22:23:32 -0700

    upload

---
+++ src/app/siakad/v/pkg1/pkg0/AppSiakadV10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.siakad.v.pkg1.pkg0;
+
+/**
+ *
+ * @author marsheila
+ */
+public class AppSiakadV10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
